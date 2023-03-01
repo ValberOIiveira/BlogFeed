cd4718d58f83b2a99b0ace76d1ba204b98a6198e
Valber Oliveira <valberoliveira789@gmail.com>
2023-03-01 12:35:14 -0300

    � Adicionado sistema de hash de senha e listagem de usuários cadastrados

--- User/UserController.js
+++ User/UserController.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./user')
+const User = require('./user');
+//Importando o bcrypt para fazer o hash de senha
+const bcrypt = require('bcryptjs');
+
 
 router.get('/admin/users',function(req,res){
-    res.send("Listagem de usuários");
+
+    User.findAll({
+        order:[
+            ['id','ASC']
+
+        ]
+    }).then(users => {
+
+        //Mostrando a lista de usuários
+        res.render('admin/User/index', {users: users});
+
+
+    })
  
 });
+
 
 
 router.get('/admin/users/create',function(req, res){
@@ -14,14 +30,39 @@ router.get('/admin/users/create',function(req, res){
 });
 
 router.post('/users/create',function(req,res){
+
+    //Nomes que colocamos nos campos do html
     var email = req.body.email;
     var password = req.body.password;
-      
-    //Testando se o form está ligado ao backend
-    res.json({email:email,password:password});
+
+
+    //Verificando se o email existe
+    User.findOne({where: {email : email}}).then( user => {
+        if(user == undefined){
+            //Fazendo o hash
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+              
+            //Testando se o form está ligado ao backend
+            // res.json({email:email,password:password});
+        
+            User.create({
+                email:email,password:hash
+            }).then(() =>{
+                res.redirect('/');
+            }).catch(error =>{
+                res.redirect('/')
+            })
+            
+
+        }else{
+            res.redirect('/admin/users/create')
+        }
+    })
+
+
     
-
-
+    
 })
 
 
